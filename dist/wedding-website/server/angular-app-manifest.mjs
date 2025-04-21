abbd76cc08ed28f7abb9e064901ebc3734055b23
ValColorado <valentina_colorado@yahoo.com>
2025-04-20 21:52:08 -0400

    deployment

--- dist/wedding-website/server/angular-app-manifest.mjs
+++ dist/wedding-website/server/angular-app-manifest.mjs
@@ -8,7 +8,7 @@ export default {
   {
     "renderMode": 2,
     "preload": [
-      "chunk-AIFAI4NP.js"
+      "chunk-Z62P36VW.js"
     ],
     "route": "/"
   },
@@ -36,12 +36,12 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 23744, hash: '35346e3ef7853cc0a1dbd565a6dfaf9a9b9c1c08dd4c7c943830231d91dbee7d', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 17318, hash: 'dc0fa9d44c10fbff40f8a826e410b24cee6e4c2bbffd7567211a39b05a02c973', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 30172, hash: '64208a15af5f307a7fdd37d4bd8db6f392911b3549cc41f2bf31bb7da386ad6d', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
-    'things-to-do/index.html': {size: 29125, hash: 'dd3cefa2e356aa6616b640821da401492218f25b35ff4c486a3f48f5d1e8cfc8', text: () => import('./assets-chunks/things-to-do_index_html.mjs').then(m => m.default)},
-    'faq/index.html': {size: 32216, hash: 'aa61ea55866c628356c2fdb4371ba69d19d9cfc22a53169a702d4e73c98ac241', text: () => import('./assets-chunks/faq_index_html.mjs').then(m => m.default)},
-    'travel/index.html': {size: 28787, hash: '518aa7103895df2b8b97bae1d3459fe2b9adcbd75ec45603048bdc12d2580602', text: () => import('./assets-chunks/travel_index_html.mjs').then(m => m.default)},
-    'styles-KUREOIK2.css': {size: 7585, hash: '+brlDQuusds', text: () => import('./assets-chunks/styles-KUREOIK2_css.mjs').then(m => m.default)}
+    'index.csr.html': {size: 23744, hash: 'fc7fa33572bfc3f7c45c3e909a3101de6da828dcd4accbe39bb631792bbda333', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 17318, hash: 'a8d4b53653d4682f5f4a3c83cf2ca8f17eda3b1881a153b090fc067edcfe1b32', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 29437, hash: '14b9c6a26b633f937ab2f20852de77587f001848e0b43c6599c443dedc8fb08c', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'faq/index.html': {size: 32255, hash: '735766a7db20564e7fd35f74a9747c48240824359ff911d89bc2126420f699fa', text: () => import('./assets-chunks/faq_index_html.mjs').then(m => m.default)},
+    'travel/index.html': {size: 28826, hash: '34e3ff1cfe644d859e621168df647e4e8fa567d2103ba6a1b1be1b23078eab4c', text: () => import('./assets-chunks/travel_index_html.mjs').then(m => m.default)},
+    'things-to-do/index.html': {size: 29164, hash: '46dfbfabcb9434e3da344505ddc8385a4f99a19343e95cd9c1158ef9aa74db7b', text: () => import('./assets-chunks/things-to-do_index_html.mjs').then(m => m.default)},
+    'styles-OBGJVZ66.css': {size: 7624, hash: 'yn71tOo5PTs', text: () => import('./assets-chunks/styles-OBGJVZ66_css.mjs').then(m => m.default)}
   },
 };
